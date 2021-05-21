4abab4092909695e190f0388f3e6a521a07a2ff2
Lessstuck <38271153+Lessstuck@users.noreply.github.com>
2021-05-20 20:23:43 -0700

    Control of patch sound through script

--- recapture/recapture/scripts/script.js
+++ recapture/recapture/scripts/script.js
@@ -3,25 +3,38 @@ const Patches = require('Patches');
 const Diagnostics = require('Diagnostics');
 const Time = require('Time');
 
+// let i = 888;
+// function loop() {
+//   // i = 999;
+//   Diagnostics.log(i);
+//   i = i + 1;
+//   Time.setTimeout(loop, 1000);
+//   return i;
+// }
+// loop();
+
 (async function () { 
 
+  // inputs from patches
   const toScriptNumber = await Patches.outputs.getScalar('toScriptNumber');
   const toScriptBoolean = await Patches.outputs.getBoolean('toScriptBoolean');
   const toScriptPulse = await Patches.outputs.getPulse('toScriptPulse')
 
   let fromScriptPulse;
   let fromScriptBoolean;
-  let fromScriptPitches;
+  let fromScriptNumber;
   
-  await Patches.inputs.setScalar('fromScriptPitches', toScriptNumber);
+  // const scalarSignal = Reactive.val(999);
+
+  //outputs to patches
+  await Patches.inputs.setScalar('fromScriptNumber', toScriptNumber);
   await Patches.inputs.setBoolean('fromScriptBoolean', toScriptBoolean);
   await Patches.inputs.setPulse('fromScriptPulse', toScriptPulse);
  
+  Diagnostics.watch('toScriptNumber =>', toScriptNumber);
+  Diagnostics.watch('fromScriptNumber =>', fromScriptNumber);
 
-  // fromScriptPitches =  await Patches.outputs.getScalar('toScriptNumber');
 
-  // Diagnostics.watch('toScriptNumber =>', toScriptNumber);
-  // Diagnostics.log(fromScriptPitches);
 
   // To access scene objects
   // const [directionalLight] = await Promise.all([
