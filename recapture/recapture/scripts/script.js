4a1cee7921496c4359804460a83eb7748b23fdb8
Lessstuck <38271153+Lessstuck@users.noreply.github.com>
2021-05-20 22:32:07 -0700

    Clean up names

--- recapture/recapture/scripts/script.js
+++ recapture/recapture/scripts/script.js
@@ -2,38 +2,22 @@ const Scene = require('Scene');
 const Patches = require('Patches');
 const Diagnostics = require('Diagnostics');
 const Time = require('Time');
+const Reactive = require('Reactive');
 
-// let i = 888;
-// function loop() {
-//   // i = 999;
-//   Diagnostics.log(i);
-//   i = i + 1;
-//   Time.setTimeout(loop, 1000);
-//   return i;
-// }
-// loop();
-
-(async function () { 
+(async function() { 
 
   // inputs from patches
-  const toScriptNumber = await Patches.outputs.getScalar('toScriptNumber');
-  const toScriptBoolean = await Patches.outputs.getBoolean('toScriptBoolean');
-  const toScriptPulse = await Patches.outputs.getPulse('toScriptPulse')
+  let toScriptNumber = await Patches.outputs.getScalar('toScriptNumber');
+  let toScriptBoolean = await Patches.outputs.getBoolean('toScriptBoolean');
+  let toScriptPulse = await Patches.outputs.getPulse('toScriptPulse')
 
   let fromScriptPulse;
   let fromScriptBoolean;
   let fromScriptNumber;
   
-  // const scalarSignal = Reactive.val(999);
-
-  //outputs to patches
-  await Patches.inputs.setScalar('fromScriptNumber', toScriptNumber);
   await Patches.inputs.setBoolean('fromScriptBoolean', toScriptBoolean);
   await Patches.inputs.setPulse('fromScriptPulse', toScriptPulse);
- 
-  Diagnostics.watch('toScriptNumber =>', toScriptNumber);
-  Diagnostics.watch('fromScriptNumber =>', fromScriptNumber);
-
+  await  Patches.inputs.setScalar('fromScriptNumber', toScriptNumber);
 
 
   // To access scene objects
